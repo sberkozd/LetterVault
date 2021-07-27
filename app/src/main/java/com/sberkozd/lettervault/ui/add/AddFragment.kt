bc69b1db2b5fc08af16e43da1ccc5c1f9c8f048a
Berk Ozdemir <sberk99@hotmail.com>
2021-07-27 17:50:06 +0300

    Date picker implementation

--- app/src/main/java/com/sberkozd/lettervault/ui/add/AddFragment.kt
+++ app/src/main/java/com/sberkozd/lettervault/ui/add/AddFragment.kt
@@ -1,9 +1,13 @@
 package com.sberkozd.lettervault.ui.add
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,9 +18,23 @@ import com.sberkozd.lettervault.databinding.FragmentAddBinding
 import com.sberkozd.lettervault.ui.add.AddViewModel
 import com.sberkozd.lettervault.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
+import java.sql.Time
+import java.util.*
 
 @AndroidEntryPoint
-class AddFragment : Fragment(R.layout.fragment_add) {
+class AddFragment : Fragment(R.layout.fragment_add), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener  {
+
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
 
     private val addViewModel: AddViewModel by viewModels()
 
@@ -46,8 +64,41 @@ class AddFragment : Fragment(R.layout.fragment_add) {
         }
 
         binding.timeIcon.setOnClickListener{
-            // datepicker implement
+            context?.let { it1 -> DatePickerDialog(it1, this, year, month, day).show() }
         }
+
+
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    private fun pickDate(){
+        getDateTimeCalendar()
+
+        DatePickerDialog(requireContext(), this, year, month, day).show()
+    }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear = year
+
+        getDateTimeCalendar()
+
+        TimePickerDialog(requireContext(),this, hour, minute, true).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
 
     }
 }
